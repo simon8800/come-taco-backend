5f1d8889dceacab90602226eff355bce2916a330
github username <github email address>
2019-06-18 20:33:24 -0400

    started Stripe

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized
 
   def create
-    puts user_login_params
     @user = User.find_by(email: user_login_params[:email])
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token(user_id: @user.id)
