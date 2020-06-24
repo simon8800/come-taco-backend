e445c64cb35193084d37e17ef90f5225c5898174
github username <github email address>
2020-06-24 13:18:16 -0400

    fixing charges controller and model

--- db/migrate/20200624171418_create_charges.rb
+++ db/migrate/20200624171418_create_charges.rb
@@ -1,8 +1,8 @@
 class CreateCharges < ActiveRecord::Migration[5.2]
   def change
     create_table :charges do |t|
+      t.float :amount
       t.string :name
-      t.integer :amount
 
       t.timestamps
     end
